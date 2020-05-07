6f08465a18b1d5670c88d6c6996b38dfbc60b1be
johannaadda <johannaadda@icloud.com>
2020-05-07 01:02:17 -0700

    final-Ex1

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Ajout d'une fonction nommée en amont dans le html via la déclaration de  L'évènement onmouseover lié à la position et au déplacement de la souris
+function setNewImage()
+{
+  document.getElementById('img1').src="assets/img/pap1.png"
+}
+
+// Ajout d'une fonction nommée en amont dans le html via la déclaration de  L'évènement  onmouseout ( sortie du pointeur de la souris)
+function setOldImage()
+{
+  document.getElementById('img1').src="assets/img/pap2.png";
+}
